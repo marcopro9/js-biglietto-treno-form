c651b9f332eb3ebb5e65bf2f05c7ab3a79ad5388
Marco <Prosperi>
2020-05-22 17:36:32 +0200

    migliorato js
    adesso ho un problema, se scrivo solo i km senza il nome mi da il ticket visibile e non mi da errore

--- script.js
+++ script.js
@@ -1,45 +1,54 @@
 // variables
   // form
-    var buttonGenera = document.getElementById('form-calcola');
-    var buttonAnnulla = document.getElementById('form-annulla');
     var formNome = document.getElementById('form-nome');
     var formKm = document.getElementById('form-km');
     var formEta = document.getElementById('form-eta');
+  // buttons
+    var buttonGenera = document.getElementById('form-calcola');
+    var buttonAnnulla = document.getElementById('form-annulla');
+  // error
+    var errore = document.getElementById('form-errore');
   // ticket
-    var sectionTicket = document.getElementById('ticket')
+    var sectionTicket = document.getElementById('ticket');
     var nomeTicket = document.getElementById('nome-ticket');
     var nomeOfferta = document.getElementById('offerta-ticket');
     var carrozza = document.getElementById('carrozza-ticket');
     var codice = document.getElementById('codicecp-ticket');
     var costo = document.getElementById('costo-ticket');
-  // general variables
-  var formKmValue;
-  var formEtaValue;
+
+
 
 // events
   buttonGenera.addEventListener("click",
-    function () {
-      formKmValue = formKm.value;
-      formEtaValue = formEta.value;
-      formNome.innerHTML = formNome.value;
-      // calc price
-      var prezzo = formKmValue * 0.21;
-      var offerta = "Prezzo intero";
-      // calc sales
-      if( formEtaValue == "under"){
-        prezzo = prezzo - ( prezzo * 20 / 100);
-        offerta = "Sconto Under 18";
-      } else if (formEtaValue == "over"){
-        prezzo = prezzo - ( prezzo * 40 / 100);
-        offerta = "Sconto Over 65";
-      }
+    function (){
+      // variables to calc costs
+      var km = parseInt(formKm.value);
+      var eta = formEta.value;
+      var nome = formNome.value;
+      // condition error
+      if (nome.lenght == 0 || isNaN(km)){
+        errore.className = "ms_visible";
+      }else {
+          // hide error
+            errore.className = "ms_hidden";
+          // variables price
+            var prezzo = km * 0.21;
+            var offerta = "Prezzo intero";
+        }
+        // calc sales
+        if (eta == "under"){
+          prezzo = prezzo - (prezzo * 20 / 100);
+          offerta = "Sconto Under 18";
+        } else if (eta == "over"){
+            prezzo = prezzo - (prezzo * 40 / 100);
+            offerta = "Sconto Over 65";
+          }
       // print on html
       nomeTicket.innerHTML = formNome.value;
       nomeOfferta.innerHTML = offerta;
       carrozza.innerHTML = Math.floor(Math.random() * 10) +1;
       codice.innerHTML = Math.random()+1;
       costo.innerHTML = prezzo.toFixed(2) + " €";
-
       // Show ticket
       sectionTicket.className = sectionTicket.classList + "ms_visible";
     }
@@ -47,12 +56,14 @@
 
   buttonAnnulla.addEventListener("click",
     function () {
+      // delete ticket on error
+      errore.className = "ms_hidden";
       // delete name
       formNome.value = "";
       nomeTicket.innerHTML = "";
       // delete km
       formKm.value = "";
-      // delete age
+      // reset age
       formEta.value = "intero";
       // delete offer
       nomeOfferta.innerHTML = "";
